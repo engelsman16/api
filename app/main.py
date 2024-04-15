eab03e829d9655e57757ca8d9ca49f9cc7fa7465
engelsman16 <teun.engels@gmail.com>
2024-04-15 21:34:52 +0200

    Added tox for test coverage

--- app/main.py
+++ app/main.py
@@ -6,4 +6,9 @@ app = FastAPI()
 async def read_main():
     return {"msg": "Hello World"}
 
+@app.get("/users/")
+async def read_users():
+    return [{"username": "Rick"}, {"username": "Morty"}]
+
+
 
